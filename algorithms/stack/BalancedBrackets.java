de9e28134b9ec460d93516556ffe07a30955abea
Gopi Shiva Krishna G <gopishiva001@outlook.com>
2020-12-21 20:41:30 +0530

    BalancedBrackets - Stacks

---
+++ algorithms/stack/BalancedBrackets.java
@@ -0,0 +1,23 @@
+package algorithms.stack;
+
+import java.util.Stack;
+
+class BalancedBrackets {
+  public static boolean balancedBrackets(String s) {
+    // Write your code here.
+    Stack<Character> stack = new Stack<>();
+
+    for (char ch : s.toCharArray()) {
+      if (ch == '(' || ch == '[' || ch == '{') stack.push(ch);
+
+      if (ch == ')' || ch == ']' || ch == '}') {
+        if (stack.empty()) return false;
+
+        char top = stack.pop();
+
+        if (ch == ')' && top != '(' || ch == ']' && top != '[' || ch == '}' && top != '{') return false;
+      }
+    }
+    return stack.empty();
+  }
+}
