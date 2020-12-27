6eb171136bc33f73b507a1e3d5ba247961371e7a
Gopi Shiva Krishna G <gopishiva001@outlook.com>
2020-12-27 17:35:24 +0530

    FourNumberSum -- Solution

--- algorithms/arrays/FourNumberSum.java
+++ algorithms/arrays/FourNumberSum.java
@@ -1,14 +1,16 @@
 package algorithms.arrays;
 
+import java.util.List;
+
 public class FourNumberSum {
   public static void main(String[] args) {
     int[] array = {7, 6, 4, -1, 1, 2};
     int targetSum = 16;
 
-    System.out.println(fourNumberSum(array, targetSum));
+    fourNumberSum(array, targetSum);
   }
 
-  private static List<Integer[]> fourNumberSum(int[] array, int targetSum){
-    
+  private static List<Integer[]> fourNumberSum(int[] array, int targetSum) {
+
   }
 }
