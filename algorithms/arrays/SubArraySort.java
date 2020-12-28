efa056e53c5acac9d76a81eaca3fb2ab8b687e52
Gopi Gorantala <ggorantala@avail.io>
2020-12-28 13:48:50 +0530

    SubArraySort -- In-Progress

---
+++ algorithms/arrays/SubArraySort.java
@@ -0,0 +1,16 @@
+package algorithms.arrays;
+
+import java.util.Arrays;
+
+public class SubArraySort {
+  public static void main(String[] args) {
+    System.out.println(Arrays.toString(subArraySort(new int[]{1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19})));
+  }
+
+  private static int[] subArraySort(int[] array) {
+    int[] result = {-1, -1};
+
+
+    return result;
+  }
+}
