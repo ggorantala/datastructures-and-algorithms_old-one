9755ce80ef7d8bf768016867eeda580c2104f73b
Gopi Shiva Krishna G <gopigorantala@MacBook-Pro.local>
2021-01-09 19:23:05 +0530

    greedy -- started

---
+++ greedy/AppleStocks.java
@@ -0,0 +1,15 @@
+package greedy;
+
+public class AppleStocks {
+  public static void main(String[] args) {
+    int[] stockPrices = new int[] {10, 7, 5, 8, 11, 9};
+
+    System.out.println(getMaxprofit(stockPrices));
+// returns 6 (buying for $5 and selling for $11)
+  }
+
+  private static int getMaxprofit(int[] stockPrices){
+
+    return 0;
+  }
+}
