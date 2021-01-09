0d70ef64ce6f98cf8b408063cf8709bbbc9bb939
Gopi Shiva Krishna G <gopigorantala@MacBook-Pro.local>
2021-01-09 19:57:50 +0530

    AppleStocks -- optimal Solution

--- greedy/AppleStocks.java
+++ greedy/AppleStocks.java
@@ -2,14 +2,29 @@ package greedy;
 
 public class AppleStocks {
   public static void main(String[] args) {
-    int[] stockPrices = new int[] {10, 7, 5, 8, 11, 9};
+    int[] stockPrices = new int[]{10, 7, 5, 8, 11, 9};
 
     System.out.println(getMaxprofit(stockPrices));
 // returns 6 (buying for $5 and selling for $11)
   }
 
-  private static int getMaxprofit(int[] stockPrices){
+  private static int getMaxprofit(int[] stockPrices) {
+    int minPrice = stockPrices[0];
+    int maxProfit = 0;
 
-    return 0;
+    for (int currentPrice : stockPrices) {
+
+      // ensure minPrice is the lowest price we've seen so far
+      minPrice = Math.min(minPrice, currentPrice);
+
+      // see what our profit would be if we bought at the
+      // min price and sold at the current price
+      int potentialProfit = currentPrice - minPrice;
+
+      // update maxProfit if we can do better
+      maxProfit = Math.max(maxProfit, potentialProfit);
+    }
+
+    return maxProfit;
   }
 }
