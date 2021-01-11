9e9ead498dda91d5a64c1ca5eade0f5272ca178e
Gopi Shiva Krishna G <gopishiva001@outlook.com>
2021-01-11 12:29:21 +0530

    FindRepeat -- In-Progress

---
+++ algorithms/search/binary/FindRepeat.java
@@ -0,0 +1,14 @@
+package algorithms.search.binary;
+
+import java.util.HashSet;
+
+public class FindRepeat {
+  public static void main(String[] args) {
+    final int[] numbers = {4, 1, 4, 8, 3, 2, 7, 6, 5};
+    System.out.println(findRepeat(numbers));
+  }
+
+  private static int findRepeat(int[] numbers){
+    // Do this later..  interviewcake
+  }
+}
